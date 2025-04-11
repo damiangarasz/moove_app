04ee0904f63fd25c28e6f831e0befe3fb014e887
damiangarasz <damian.garasz@gmail.com>
2025-04-11 18:11:27 +0200

    lol

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,9 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [,],
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
   };
 };
-
-["babel-preset-expo", { jsxImportSource: "nativewind" }], "nativewind/babel";
